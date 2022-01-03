18b2559ac69c8e9d2cb1d7c9974ed811597818dd
Anıl <anilakkaya58@gmail.com>
2022-01-03 19:34:40 +0300

    Initial Commit

---
+++ SwiftUI-Pokemon-VIPER/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Pokemon-VIPER
+//
+//  Created by Joshua on 3.01.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Pokemon-VIPER/SwiftUI_Pokemon_VIPERApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Pokemon_VIPERApp.swift
+//  SwiftUI-Pokemon-VIPER
+//
+//  Created by Joshua on 3.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Pokemon_VIPERApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
